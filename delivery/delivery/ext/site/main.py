ed5c7deba85457e7de33ce907b1a18bd30664cc7
Cley Randal <cleyrandal@gmail.com>
2022-09-04 13:49:01 -0300

    :sparkles: Add new route
    Add "sobre" route

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -7,3 +7,8 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
